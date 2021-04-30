3e07712060ef6c4a99bf642c88081471b84b1d07
Ereyomi Oluwaseyi <ereyomioluwaseyi@gmail.com>
2021-04-30 17:07:26 +0100

    feat: custom radio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { InputComponent } from './shared/input/input.component';
+import { CardComponent } from './shared/card/card.component';
+import { RadioInputComponent } from './shared/radio-input/radio-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    InputComponent,
+    CardComponent,
+    RadioInputComponent
   ],
   imports: [
     BrowserModule,
